237024782d549afa6e1d5075b785358640eb6031
XxpichoclesxX <81063926+XxpichoclesxX@users.noreply.github.com>
2022-02-02 16:38:49 -0600

    Add files via upload

---
+++ Sportmode.lua
@@ -0,0 +1,164 @@
+-- Sportmode Script
+-- Version: 1.1
+-- Original Author: jayphen#6666
+-- Modified by: Hecker#6976
+
+
+PED = {
+	["GET_VEHICLE_PED_IS_IN"]=function(--[[Ped (int)]] ped,--[[BOOL (bool)]] includeLastVehicle)native_invoker.begin_call();native_invoker.push_arg_int(ped);native_invoker.push_arg_bool(includeLastVehicle);native_invoker.end_call("9A9112A0FE9A4713");return native_invoker.get_return_value_int();end,
+}
+PLAYER = {
+	["PLAYER_PED_ID"]=function()native_invoker.begin_call();native_invoker.end_call("D80958FC74E988A6");return native_invoker.get_return_value_int();end,
+}
+CAM = {
+	["GET_GAMEPLAY_CAM_ROT"]=function(--[[int]] rotationOrder)native_invoker.begin_call();native_invoker.push_arg_int(rotationOrder);native_invoker.end_call("837765A25378F0BB");return native_invoker.get_return_value_vector3();end,
+	["SET_CAM_ROT"]=--[[void]] function(--[[Cam (int)]] cam,--[[float]] rotX,--[[float]] rotY,--[[float]] rotZ,--[[int]] rotationOrder)native_invoker.begin_call();native_invoker.push_arg_int(cam);native_invoker.push_arg_float(rotX);native_invoker.push_arg_float(rotY);native_invoker.push_arg_float(rotZ);native_invoker.push_arg_int(rotationOrder);native_invoker.end_call("85973643155D0B07");end,
+	["_SET_GAMEPLAY_CAM_RELATIVE_ROTATION"]=--[[void]] function(--[[float]] roll,--[[float]] pitch,--[[float]] yaw)native_invoker.begin_call();native_invoker.push_arg_float(roll);native_invoker.push_arg_float(pitch);native_invoker.push_arg_float(yaw);native_invoker.end_call("48608C3464F58AB4");end,
+
+}
+ENTITY = {
+	["SET_ENTITY_ROTATION"]=function(--[[Entity (int)]] entity,--[[float]] pitch,--[[float]] roll,--[[float]] yaw,--[[int]] rotationOrder,--[[BOOL (bool)]] p5)native_invoker.begin_call();native_invoker.push_arg_int(entity);native_invoker.push_arg_float(pitch);native_invoker.push_arg_float(roll);native_invoker.push_arg_float(yaw);native_invoker.push_arg_int(rotationOrder);native_invoker.push_arg_bool(p5);native_invoker.end_call("8524A8B0171D5E07");end,
+	["SET_ENTITY_COLLISION"]=function(--[[Entity (int)]] entity,--[[BOOL (bool)]] toggle,--[[BOOL (bool)]] keepPhysics)native_invoker.begin_call();native_invoker.push_arg_int(entity);native_invoker.push_arg_bool(toggle);native_invoker.push_arg_bool(keepPhysics);native_invoker.end_call("1A9205C1B9EE827F");end,
+	["APPLY_FORCE_TO_ENTITY"]=function(--[[Entity (int)]] entity,--[[int]] forceFlags,--[[float]] x,--[[float]] y,--[[float]] z,--[[float]] offX,--[[float]] offY,--[[float]] offZ,--[[int]] boneIndex,--[[BOOL (bool)]] isDirectionRel,--[[BOOL (bool)]] ignoreUpVec,--[[BOOL (bool)]] isForceRel,--[[BOOL (bool)]] p12,--[[BOOL (bool)]] p13)native_invoker.begin_call();native_invoker.push_arg_int(entity);native_invoker.push_arg_int(forceFlags);native_invoker.push_arg_float(x);native_invoker.push_arg_float(y);native_invoker.push_arg_float(z);native_invoker.push_arg_float(offX);native_invoker.push_arg_float(offY);native_invoker.push_arg_float(offZ);native_invoker.push_arg_int(boneIndex);native_invoker.push_arg_bool(isDirectionRel);native_invoker.push_arg_bool(ignoreUpVec);native_invoker.push_arg_bool(isForceRel);native_invoker.push_arg_bool(p12);native_invoker.push_arg_bool(p13);native_invoker.end_call("C5F68BE9613E2D18");end,
+	["FREEZE_ENTITY_POSITION"]=function(--[[Entity (int)]] entity,--[[BOOL (bool)]] toggle)native_invoker.begin_call();native_invoker.push_arg_int(entity);native_invoker.push_arg_bool(toggle);native_invoker.end_call("428CA6DBD1094446");end,
+}
+VEHICLE = {
+	["SET_VEHICLE_FORWARD_SPEED"]=function(--[[Vehicle (int)]] vehicle,--[[float]] speed)native_invoker.begin_call();native_invoker.push_arg_int(vehicle);native_invoker.push_arg_float(speed);native_invoker.end_call("AB54A438726D25D5");end,
+	["SET_VEHICLE_GRAVITY"]=function(--[[Vehicle (int)]] vehicle,--[[BOOL (bool)]] toggle)native_invoker.begin_call();native_invoker.push_arg_int(vehicle);native_invoker.push_arg_bool(toggle);native_invoker.end_call("89F149B6131E57DA");end,
+	["SET_VEHICLE_EXTRA_COLOURS"]=--[[void]] function(--[[Vehicle (int)]] vehicle,--[[int]] pearlescentColor,--[[int]] wheelColor)native_invoker.begin_call();native_invoker.push_arg_int(vehicle);native_invoker.push_arg_int(pearlescentColor);native_invoker.push_arg_int(wheelColor);native_invoker.end_call("2036F561ADD12E33");end,
+
+}
+PAD = {
+	["IS_CONTROL_PRESSED"]=function(--[[int]] padIndex,--[[int]] control)native_invoker.begin_call();native_invoker.push_arg_int(padIndex);native_invoker.push_arg_int(control);native_invoker.end_call("F3A21BCD95725A4A");return native_invoker.get_return_value_bool();end,
+}
+veh = PED.GET_VEHICLE_PED_IS_IN(PLAYER.PLAYER_PED_ID(), false);
+local is_vehicle_flying = false
+local dont_stop = false
+local no_collision = false
+local speed = 6
+local reset_veloicty = false
+
+util.toast("impulse-like sport mode loaded.")
+util.toast("full WASD controls. you can also hold shift to boost the speed a little bit :)")
+
+menu.toggle(menu.my_root(), "Vehicle fly", {"vehfly"}, "I recommend setting a hotkey to this option.", function(on_click)
+    is_vehicle_flying = on_click
+    if reset_veloicty then 
+        ENTITY.FREEZE_ENTITY_POSITION(veh, true)
+        util.yield()
+        ENTITY.FREEZE_ENTITY_POSITION(veh, false)
+    end
+end)
+menu.slider(menu.my_root(), "Speed", {"speed"}, "", 1, 100, 6, 1, function(on_change) 
+    speed = on_change
+end)
+menu.toggle(menu.my_root(), "Don't stop after input", {"dontstop"}, "", function(on_click)
+    dont_stop = on_click
+end)
+menu.toggle(menu.my_root(), "Reset velocity on toggle", {"resetvehflyvelocity"}, "So you don't go flying after turning this off while still moving", function(on_click)
+    reset_veloicty = on_click
+end)
+menu.toggle(menu.my_root(), "No collision", {"nocolision"}, "", function(on_click)
+    no_collision = on_click
+end)
+
+
+vehicleroll = 0
+
+
+function do_vehicle_fly() 
+    veh = PED.GET_VEHICLE_PED_IS_IN(PLAYER.PLAYER_PED_ID(), false);
+    cam_pos = CAM.GET_GAMEPLAY_CAM_ROT(0);
+    ENTITY.SET_ENTITY_ROTATION(veh, cam_pos.x, vehicleroll, cam_pos.z, 2, true)
+    ENTITY.SET_ENTITY_COLLISION(veh, not no_collision, true);
+    if PAD.IS_CONTROL_PRESSED(0, 108) then 
+        vehicleroll = vehicleroll -1
+    end
+
+    if PAD.IS_CONTROL_PRESSED(0, 109) then 
+        vehicleroll = vehicleroll +1
+       
+    end
+
+    local locspeed = speed*10
+    local locspeed2 = speed
+    if PAD.IS_CONTROL_PRESSED(0, 61) then 
+        locspeed = locspeed*2
+        locspeed2 = locspeed2*2
+    end
+
+    
+    if PAD.IS_CONTROL_PRESSED(2, 71) then
+        if dont_stop then
+            ENTITY.APPLY_FORCE_TO_ENTITY(veh, 1, 0.0, speed, 0.0, 0.0, 0.0, 0.0, 0, 1, 1, 1, 0, 1)
+        else 
+            VEHICLE.SET_VEHICLE_FORWARD_SPEED(veh, locspeed)
+        end
+	end
+    if PAD.IS_CONTROL_PRESSED(2, 72) then
+		local lsp = speed
+        if not PAD.IS_CONTROL_PRESSED(0, 61) then 
+            lsp = speed * 2
+        end
+        if dont_stop then
+            ENTITY.APPLY_FORCE_TO_ENTITY(veh, 1, 0.0, 0 - (lsp), 0.0, 0.0, 0.0, 0.0, 0, 1, 1, 1, 0, 1)
+        else 
+            VEHICLE.SET_VEHICLE_FORWARD_SPEED(veh, 0 - (locspeed));
+        end
+   end
+    if PAD.IS_CONTROL_PRESSED(2, 63) then
+        local lsp = (0 - speed)*2
+        if not PAD.IS_CONTROL_PRESSED(0, 61) then 
+            lsp = 0 - speed
+        end
+        if dont_stop then
+            ENTITY.APPLY_FORCE_TO_ENTITY(veh, 1, (lsp), 0.0, 0.0, 0.0, 0.0, 0.0, 0, 1, 1, 1, 0, 1)
+        else 
+            ENTITY.APPLY_FORCE_TO_ENTITY(veh, 1, 0 - (locspeed), 0.0, 0.0, 0.0, 0.0, 0.0, 0, 1, 1, 1, 0, 1);
+        end
+	end
+    if PAD.IS_CONTROL_PRESSED(2, 64) then
+        local lsp = speed
+        if not PAD.IS_CONTROL_PRESSED(0, 61) then 
+            lsp = speed*2
+        end
+        if dont_stop then
+            ENTITY.APPLY_FORCE_TO_ENTITY(veh, 1, lsp, 0.0, 0.0, 0.0, 0.0, 0.0, 0, 1, 1, 1, 0, 1)
+        else 
+            ENTITY.APPLY_FORCE_TO_ENTITY(veh, 1, locspeed, 0.0, 0.0, 0.0, 0.0, 0.0, 0, 1, 1, 1, 0, 1)
+        end
+    end
+	if not dont_stop and not PAD.IS_CONTROL_PRESSED(2, 71) and not PAD.IS_CONTROL_PRESSED(2, 72) then
+        VEHICLE.SET_VEHICLE_FORWARD_SPEED(veh, 0.0);
+    end
+end
+
+
+util.create_tick_handler(function() 
+
+
+    -- Added by LAZ
+    if PED.GET_VEHICLE_PED_IS_IN(PLAYER.PLAYER_PED_ID(),false) == 0 then
+        if is_vehicle_flying then
+            menu.trigger_commands("vehfly")
+            util.toast("Disabled Sport mode. Not in vehicle")
+        end
+    else
+        if is_vehicle_flying then do_vehicle_fly() end
+
+
+    end
+
+    -- End Added by LAZ
+
+    VEHICLE.SET_VEHICLE_GRAVITY(veh, not is_vehicle_flying) 
+    if not is_vehicle_flying then 
+        ENTITY.SET_ENTITY_COLLISION(veh, true, true);
+    end
+
+    return true
+end)
+util.on_stop(function() 
+    VEHICLE.SET_VEHICLE_GRAVITY(veh, true)
+    ENTITY.SET_ENTITY_COLLISION(veh, true, true);
+end)
